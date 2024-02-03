37a0d2bb6d46fb23093bff27c369f4b20f845979
moisesguedes <moises.guedes.viaflow@segurosunimed.com.br>
2024-02-03 16:36:16 -0300

    [project structure] setup env variables with zod validation.
    -install dotenv
    -install zod

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+import { env } from "./env";
 import { app } from "./app";
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
     host: "0.0.0.0" //to avoid issues with any client that connects to this server
 }).then(() => {
     console.log("🎃 Server listening on port 3333");
