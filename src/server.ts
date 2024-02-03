973ac6a0a0eb22e70b3bf9f91a2903de845c2377
moisesguedes <moises.guedes.viaflow@segurosunimed.com.br>
2024-02-03 16:53:03 -0300

    [project structure] setup eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { env } from "./env";
-import { app } from "./app";
+import { env } from './env'
+import { app } from './app'
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
-    host: "0.0.0.0" //to avoid issues with any client that connects to this server
-}).then(() => {
-    console.log("🎃 Server listening on port 3333");
-})
+    host: '0.0.0.0', // to avoid issues with any client that connects to this server
+  })
+  .then(() => {
+    console.log('🎃 Server listening on port 3333')
+  })
