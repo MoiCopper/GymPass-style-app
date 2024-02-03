7eba416afbf35b7a8ff31ee4c6b79cdd4e7c782a
moisesguedes <moises.guedes.viaflow@segurosunimed.com.br>
2024-02-03 18:54:06 -0300

    [feature] create register controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
-const prismas = new PrismaClient()
-
+import { appRoutes } from './http/routes'
 export const app = fastify()
+
+app.register(appRoutes)
